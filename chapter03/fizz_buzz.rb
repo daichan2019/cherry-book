fcea216dde6aae0f60f38c9376bef175a9462227
daichi_sato <daichiexperience@gmail.com>
2022-07-11 22:15:34 +0900

    [add] minitest to fizz_buzz program

--- chapter03/fizz_buzz.rb
+++ chapter03/fizz_buzz.rb
@@ -10,10 +10,16 @@ def fizz_buzz(n)
   end
 end
 
-# pp fizz_buzz(1)
-# pp fizz_buzz(2)
-# pp fizz_buzz(3)
-# pp fizz_buzz(4)
-# pp fizz_buzz(5)
-# pp fizz_buzz(6)
-# pp fizz_buzz(15)
+require 'minitest/autorun'
+
+class FizzBuzzTest < Minitest::Test
+  def test_fizz_buzz
+    assert_equal '1', fizz_buzz(1)
+    assert_equal '2', fizz_buzz(2)
+    assert_equal 'Fizz', fizz_buzz(3)
+    assert_equal '4', fizz_buzz(4)
+    assert_equal 'Buzz', fizz_buzz(5)
+    assert_equal 'Fizz', fizz_buzz(6)
+    assert_equal 'Fizz Buzz', fizz_buzz(15)
+  end
+end
